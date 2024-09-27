ba33c53fc9e8c6dd1d4f41d1aa9305080a256903
nuocwang <wang.nuoc@northeastern.edu>
2024-09-26 20:59:38 -0400

    fix merge

--- Lab2/lab2_hz2994_nw2568_tz2642_check2.py
+++ Lab2/lab2_hz2994_nw2568_tz2642_check2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from machine import Pin, PWM, ADC, Timer
 import time, builtins
 
@@ -39,46 +38,4 @@ button_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=button_isr)
 # Main loop
 while True:
     builtins.print(button_pressed)
-=======
-from machine import Pin, PWM, ADC, Timer
-import time, builtins
-
-# Setup pins
-button_pin = Pin(15, Pin.IN, Pin.PULL_UP)  # GPIO15 as input with pull-up
-
-# Timer for debounce
-debounce_timer = Timer(-1)
-
-# Debounce time in milliseconds
-DEBOUNCE_TIME = 200
-
-# System state
-button_pressed = False
-
-# Interrupt Service Routine for the button
-def button_isr(pin):
-    global button_pressed
-    # Disable the interrupt temporarily
-    button_pin.irq(handler=None)
-    
-    # Start debounce timer
-    debounce_timer.init(mode=Timer.ONE_SHOT, period=DEBOUNCE_TIME, callback=debounce_callback)
-
-def debounce_callback(timer):
-    global button_pressed
-
-    # Re-enable interrupt after debounce
-    if button_pin.value():  # Button pressed (value is 0)
-        button_pressed = True
-    else:
-        button_pressed = False
-    
-    button_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=button_isr)
-# Attach interrupt to the button (falling and rising edge)
-button_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=button_isr)
-
-# Main loop
-while True:
-    builtins.print(button_pressed)
->>>>>>> 6581ca2172c6029f89173e89b8a9b4d944d692a5
     time.sleep(0.1)
--- Lab2/lab2_hz2994_nw2568_tz2642_check3.py
+++ Lab2/lab2_hz2994_nw2568_tz2642_check3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from machine import Pin, PWM, ADC, Timer
 import time, builtins
 FREQUENCY = 256
@@ -61,68 +60,4 @@ while True:
         led_pin.duty(0)  # Turn off the LED when the button is released
         pwd_piezzo.duty(0)
     # builtins.print(button_pressed)
-=======
-from machine import Pin, PWM, ADC, Timer
-import time, builtins
-FREQUENCY = 256
-#sampling frequency 1 hz
-SAMP_FREQ = 0.1
-
-# Setup pins
-button_pin = Pin(15, Pin.IN, Pin.PULL_UP)  # GPIO15 as input with pull-up
-# button_pin.value(1)
-led_pin = PWM(Pin(12, Pin.OUT), freq=FREQUENCY)  # GPIO13 as PWM for LED
-light_sensor = ADC(0)  # ADC for light sensor
-piezzo = Pin(13, Pin.OUT)
-pwd_piezzo = PWM(piezzo, freq=FREQUENCY)
-
-# Timer for debounce
-debounce_timer = Timer(-1)
-
-# Debounce time in milliseconds
-DEBOUNCE_TIME = 200
-
-# System state
-button_pressed = False
-
-# Interrupt Service Routine for the button
-def button_isr(pin):
-    global button_pressed
-    # Disable the interrupt temporarily
-    button_pin.irq(handler=None)
-    
-    # Start debounce timer
-    debounce_timer.init(mode=Timer.ONE_SHOT, period=DEBOUNCE_TIME, callback=debounce_callback)
-
-def debounce_callback(timer):
-    global button_pressed
-    # builtins.print('True2')
-    # Re-enable interrupt after debounce
-    if button_pin.value():  # Button pressed (value is 0)
-        button_pressed = True
-        # while True:
-        #     builtins.print('True1')
-        #     light_value = light_sensor.read()  # ADC value (0-1023)
-        #     led_pin.duty(light_value)  # Set the brightness of LED (PWM duty cycle)
-    else:
-        button_pressed = False
-        # led_pin.duty(0)
-    
-    button_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=button_isr)
-# Attach interrupt to the button (falling and rising edge)
-button_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=button_isr)
-
-# Main loop
-while True:
-    if button_pressed:
-        # Read from the light sensor
-        light_value = light_sensor.read()  # ADC value (0-1023)
-        led_pin.duty(light_value)  # Set the brightness of LED (PWM duty cycle)
-        pwd_piezzo.duty(light_value //10)
-        builtins.print(light_value //10)
-    else:
-        led_pin.duty(0)  # Turn off the LED when the button is released
-        pwd_piezzo.duty(0)
-    # builtins.print(button_pressed)
->>>>>>> 6581ca2172c6029f89173e89b8a9b4d944d692a5
     time.sleep(SAMP_FREQ)
